6d54e5e69ea64b900e1a4c90dd50d907f7f12d01
spainchesterdaeg <spainchester@outlook.com>
2020-10-01 13:56:09 -0500

    ejercicio3 implementado

--- js/main_ej3.js
+++ js/main_ej3.js
@@ -1,21 +1,29 @@
 function imprimirApariciones(){
     var cadena = document.getElementById('caja_cadena').value;
+    var caracter = document.getElementById('caja_caracter').value;
     var cajaVocales = document.getElementById('caja_vocales');
     var cajaConsonanes = document.getElementById('caja_consonantes');
     var cajaAparicion = document.getElementById('caja_aparicion');
     var cajaCadenaLoca = document.getElementById('caja_cadena_loca');
 
+
+
+
     var resultadoVocales = contarVocales(cadena);
-    console.log(resultadoVocales);
+    cajaVocales.value = resultadoVocales
+    //console.log(resultadoVocales);
 
     var resultadoConsonantes = contarConsonantes(cadena);
-    console.log(resultadoConsonantes);
+    cajaConsonanes.value = resultadoConsonantes;
+    //console.log(resultadoConsonantes);
 
-    var resultadoAparicion = contarCaracteres(cadena,"e");
-    console.log(resultadoAparicion);
+    var resultadoAparicion = contarCaracteres(cadena,caracter);
+    cajaAparicion.value = resultadoAparicion;
+    //console.log(resultadoAparicion);
 
     var resultadoCadenaLoca = obtenerCadenaLoca(cadena);
-    console.log(resultadoCadenaLoca);
+    cajaCadenaLoca.value = resultadoCadenaLoca;
+    //console.log(resultadoCadenaLoca);
 
     
 }
